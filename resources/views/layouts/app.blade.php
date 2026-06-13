70ca1c994789835dd2b058e8d1d4086cea818e3d
David Ngungila <davidngungila@gmail.com>
2026-06-13 20:54:52 +0300

    Update sidebar to use feedtanstorelogo.png and add white text stroke

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -236,10 +236,14 @@
     <!-- Sidebar Header -->
     <div class="flex items-center justify-between p-4 border-b border-white/20 flex-shrink-0">
       <div class="flex items-center gap-3" x-show="!sidebarCollapsed || window.innerWidth<1024">
-        <img src="{{ asset('feedtanstorelogo.png') }}" alt="FEEDTAN STORE" class="w-10 h-10 rounded-lg object-cover flex-shrink-0 border-2 border-white">
+        <img src="{{ asset('feedtanstorelogo.png') }}" alt="FEEDTAN STORE" class="w-10 h-10 rounded-lg flex-shrink-0 object-contain">
+        <div x-show="!sidebarCollapsed">
+          <p class="text-white font-bold text-sm leading-tight" style="-webkit-text-stroke: 0.5px white; text-stroke: 0.5px white;">FEEDTAN</p>
+          <p class="text-primary-300 text-[10px]" style="-webkit-text-stroke: 0.3px white; text-stroke: 0.3px white;">STORE</p>
+        </div>
       </div>
-      <div x-show="sidebarCollapsed && window.innerWidth>=1024" class="mx-auto">
-        <img src="{{ asset('feedtanstorelogo.png') }}" alt="FEEDTAN STORE" class="w-8 h-8 rounded-lg object-cover border-2 border-white">
+      <div x-show="sidebarCollapsed && window.innerWidth>=1024" class="w-10 h-10 rounded-lg flex items-center justify-center mx-auto">
+        <img src="{{ asset('feedtanstorelogo.png') }}" alt="FEEDTAN STORE" class="w-full h-full rounded-lg object-contain">
       </div>
       <button @click="sidebarCollapsed=!sidebarCollapsed" class="text-primary-300 hover:text-white transition-colors hidden lg:block">
         <i :class="sidebarCollapsed?'fa-solid fa-chevron-right':'fa-solid fa-chevron-left'" class="text-xs"></i>
