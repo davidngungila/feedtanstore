c991ec854adb4d8a384ef76c6dd176ea63bd2e5f
David Ngungila <davidngungila@gmail.com>
2026-06-18 17:02:22 +0300

    Fix route name for finance settings update

--- resources/views/finance/settings.blade.php
+++ resources/views/finance/settings.blade.php
@@ -13,7 +13,7 @@
             </div>
         @endif
 
-        <form action="{{ route('settings.update') }}" method="POST">
+        <form action="{{ route('store.settings.update') }}" method="POST">
             @csrf
             @method('PUT')
 
