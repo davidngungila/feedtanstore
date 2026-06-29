cdaf122cef90fdccfc51eeb9d71679a60887bc18
David Ngungila <davidngungila@gmail.com>
2026-06-29 06:46:54 +0300

    Move online order notifications to background job to prevent page loading delays

--- app/Http/Controllers/OnlineOrderController.php
+++ app/Http/Controllers/OnlineOrderController.php
@@ -22,52 +22,7 @@ use App\Services\MessagingService;
 
 class OnlineOrderController extends Controller
 {
-    protected function sendPublicOrderNotifications(OnlineOrder $order): void
-    {
-        $trackingUrl = url('/shop/tracking/' . $order->order_number);
-        $pdfUrl = url('/shop/tracking/' . $order->order_number . '/pdf');
 
-        $smsProfile = CommunicationProfile::where('type', 'sms')->where('is_active', true)->first();
-        if ($smsProfile && $order->customer_phone) {
-            $phoneNumber = $order->customer_phone;
-            if (substr($phoneNumber, 0, 1) === '0') {
-                $phoneNumber = '255' . substr($phoneNumber, 1);
-            }
-            try {
-                $smsText = "Thanks {$order->customer_name}. Your order {$order->order_number} has been received. Delivery Code: {$order->delivery_code}. Please keep this code safe and provide it upon delivery. Track: {$trackingUrl}";
-                $messagingService = new MessagingService($smsProfile->sms_api_key, $smsProfile->messaging_sender_id, false);
-                $messagingService->sendSms($phoneNumber, $smsText);
-            } catch (\Exception $e) {
-                \Log::error('Failed to send online order SMS: ' . $e->getMessage());
-            }
-        }
-
-        $emailProfile = CommunicationProfile::where('type', 'email')->where('is_active', true)->first();
-        if ($emailProfile && $order->customer_email) {
-            config([
-                'mail.mailers.test_smtp' => [
-                    'transport' => 'smtp',
-                    'host' => $emailProfile->smtp_host,
-                    'port' => $emailProfile->smtp_port,
-                    'encryption' => $emailProfile->smtp_encryption,
-                    'username' => $emailProfile->smtp_username,
-                    'password' => $emailProfile->smtp_password,
-                    'timeout' => 30,
-                    'local_domain' => null,
-                ],
-                'mail.from' => [
-                    'address' => $emailProfile->email_from_address,
-                    'name' => $emailProfile->email_from_name,
-                ],
-            ]);
-
-            try {
-                Mail::mailer('test_smtp')->to($order->customer_email)->send(new OnlineOrderPlaced($order));
-            } catch (\Exception $e) {
-                \Log::error('Failed to send online order email: ' . $e->getMessage());
-            }
-        }
-    }
 
     public function initiatePaymentForOrder(Request $request, $orderNumber, FeedtanEcommercePaymentService $paymentService)
     {
@@ -675,7 +630,7 @@ class OnlineOrderController extends Controller
             'notes' => 'Order placed from public shop'
         ]);
 
-        $this->sendPublicOrderNotifications($order);
+        \App\Jobs\SendOnlineOrderNotifications::dispatch($order);
 
         $paymentResponse = null;
         $paymentInitiated = false;
