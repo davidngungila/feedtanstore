940b9b7e59a4baa3b3c151d73e90307f8f92d0d3
David Ngungila <davidngungila@gmail.com>
2026-06-29 22:05:14 +0300

    Redirect unauthenticated users to /entry instead of /login
    - Configure middleware to redirect guests to route('entry')
    - Session expiration now directs to /entry page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(function () {
+            return route('entry');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
