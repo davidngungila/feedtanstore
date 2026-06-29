3542b9a984db308e7df95b7b6d2096e0a620ae36
David Ngungila <davidngungila@gmail.com>
2026-06-29 15:53:51 +0300

    Fix map display for other location selection
    - Remove display:none from mapPreviewOther element
    - Map now shows immediately when Other Location is selected
    - Remove manual display:block setting in toggle function
    - Improve user experience by showing map right away

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -482,7 +482,7 @@ footer{background:var(--green-900);color:#BFD6C8;padding:40px 0 0;margin-top:40p
               </div>
             </div>
             <div class="loc-coords" id="mapLocCoords"></div>
-            <div class="mini-map" id="mapPreviewOther" style="display:none;">
+            <div class="mini-map" id="mapPreviewOther">
               <div class="pin">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
               </div>
@@ -742,7 +742,6 @@ function toggleLocationType() {
     otherLocationBox.style.display = 'block';
     optOther.classList.add('selected');
     optCurrent.classList.remove('selected');
-    document.getElementById('mapPreviewOther').style.display = 'block';
     initializeCheckoutMapOther();
     setTimeout(() => {
       if (checkoutMapOther) checkoutMapOther.invalidateSize();
