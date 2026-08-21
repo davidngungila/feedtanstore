0d348141f9e3b24a252b076e8617970f4fcc81f5
David Ngungila <davidngungila@gmail.com>
2026-08-21 15:24:09 +0300

    Add GC, DC, ZNUM to TRA VFD XML payload

--- app/Services/TraVfdService.php
+++ app/Services/TraVfdService.php
@@ -182,6 +182,11 @@ class TraVfdService
             'permitNum' => $permitNum,
         ]);
 
+        // Get fiscal counters
+        $gc = (int) ($settings->tra_gc ?? 1);
+        $dc = (int) ($settings->tra_dc ?? 1);
+        $znum = $settings->tra_znum ?? date('Ymd');
+
         $dataXml = "<ITEMS>{$itemsXml}</ITEMS>"
             . "<Serial_num>{$this->vfdSerial}</Serial_num>"
             . "<TIN_num>{$this->tinNumber}</TIN_num>"
@@ -192,6 +197,9 @@ class TraVfdService
             . "<permitNum>{$permitNum}</permitNum>"
             . "<vatAmt>{$vatAmt}</vatAmt>"
             . "<grossAmt>{$grossAmt}</grossAmt>"
+            . "<GC>{$gc}</GC>"
+            . "<DC>{$dc}</DC>"
+            . "<ZNUM>{$znum}</ZNUM>"
             . "<CASH>{$cash}</CASH>"
             . "<CHEQUE>{$cheque}</CHEQUE>"
             . "<CCARD>{$ccard}</CCARD>"
